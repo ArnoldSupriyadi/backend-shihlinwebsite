fc380f5b38f30181e12069062dc298c90c09fe42
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-04-13 12:38:36 +0700

    first commit

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,17 @@
+<?php
+if (! function_exists('TanggalID')) {         
+    
+    /**
+     * TanggalID
+     *
+     * @param  mixed $tanggal
+     * @return void
+     */
+    function TanggalID($tanggal) {
+        $value = Carbon\Carbon::parse($tanggal);
+        $parse = $value->locale('id');
+        return $parse->translatedFormat('l, d F Y');
+    }
+}
+
+?>
